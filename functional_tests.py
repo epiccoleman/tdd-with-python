8e013c83fbd4ea42d216a2817e3a6b44690daf8c
Eric Coleman <eric@epiccoleman.com>
2019-05-15 23:48:02 -0400

    sketch out static part of home page

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -36,7 +37,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any (row.text == '1: Buy concert tickets' for row in rows)
+            any (row.text == '1: Buy concert tickets' for row in rows),
+            "New to-do item did not appear in table"
         )
 
         # the user is prompted to enter another to do item. they enter "attend concert"
